75503cbf95d1542bb86757c13e1d667a7ac65936
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-10 19:16:50 +0000

    Create page for each title and member url

--- run.py
+++ run.py
@@ -1,28 +1,46 @@
 import sys, subprocess
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask'])
 from flask import Flask,render_template
-import os
+import os,json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index(): # also called view
-    return render_template('index.html')
+    return render_template('index.html', page_title="Home",)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    data=[]
+    with open('data/company.json','r') as json_data:
+        data = json.load(json_data)
+
+    return render_template('about.html', page_title="About",company = data)
+
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = []
+    with open('data/company.json', 'r') as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj['url'] == member_name:
+                member = obj
+
+    return render_template('member.html', member=member)
+
 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template('careers.html')
+    return render_template('careers.html',page_title="Careers")
 
 
 if __name__ == "__main__":
