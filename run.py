7124698ac8906b829c0be57abb7b0ff56e984280
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-01-10 10:28:52 +0000

    Run flask and add 4 templates page

---
+++ run.py
@@ -0,0 +1,34 @@
+import sys, subprocess
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask'])
+from flask import Flask,render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): # also called view
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/careers")
+def careers():
+    return render_template('careers.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        host = os.environ.get("IP","0.0.0.0"),
+        port = int(os.environ.get("PORT","5000")),
+        debug = True
+    )
+    
